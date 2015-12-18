40e17111768fcc17d8ac34d05c8857050dbb40c8
Joao Junior <joaojunior.ma@gmail.com>
2015-12-18 14:29:59 -0500

    Add case test 2

--- spolm/CPTTRN1/cpttrn1.py
+++ spolm/CPTTRN1/cpttrn1.py
@@ -1,2 +1,17 @@
 def resolve(lines, collumns):
-    return '*\n.\n*'
+    result = ''
+    for line in xrange(lines):
+        for collumn in xrange(collumns):
+            line_rest = line % 2
+            collumn_rest = collumn % 2
+            if line_rest == 0 and collumn_rest == 0:
+                result += '*'
+            elif line_rest == 0 and collumn_rest != 0:
+                result += '.'
+            elif line_rest != 0 and collumn_rest == 0:
+                result += '.'
+            elif line_rest != 0 and collumn_rest != 0:
+                result += '*'
+        if line + 1 != lines:
+            result += '\n'
+    return result
--- spolm/CPTTRN1/tests.py
+++ spolm/CPTTRN1/tests.py
@@ -6,5 +6,7 @@ class TestCPTTRN1(unittest.TestCase):
     def test_first_example(self):
         self.assertEqual('*\n.\n*', resolve(lines=3, collumns=1))
 
+    def test_seccond_example(self):
+        self.assertEqual('*.*.\n.*.*\n*.*.\n.*.*', resolve(lines=4, collumns=4))
 if __name__ == '__main__':
     unittest.main()
