8cff3d1ce33a2a3978085e7dad10c9e7c072ea8d
Joao Junior <joaojunior.ma@gmail.com>
2015-12-18 17:04:25 -0500

    Add seccond test case

--- spolm/HS12MBR/hs12mbr.py
+++ spolm/HS12MBR/hs12mbr.py
@@ -1,2 +1,25 @@
 def resolve(points=(), lines=(), circles=()):
-    return '3 3 3 3'
+    x_min = 1000
+    x_max = -1000
+    y_min = 1000
+    y_max = -1000
+    for point in points:
+        if point[0] < x_min:
+            x_min = point[0]
+        if point[0] > x_max:
+            x_max = point[0]
+        if point[1] < y_min:
+            y_min = point[1]
+        if point[1] > y_max:
+            y_max = point[1]
+    for circle in circles:
+        ray = circle[2]
+        if circle[0] - ray < x_min:
+            x_min = circle[0] - ray
+        if circle[1] - ray < y_min:
+            y_min = circle[1] - ray
+        if circle[0] + ray > x_max:
+            x_max = circle[0] + ray
+        if circle[1] + ray > y_max:
+            y_max = circle[1] + ray
+    return '{0} {1} {2} {3}'.format(x_min, y_min, x_max, y_max)
--- spolm/HS12MBR/tests.py
+++ spolm/HS12MBR/tests.py
@@ -4,7 +4,10 @@ from hs12mbr import resolve
 
 class HS12MBR(unittest.TestCase):
     def test_first_example(self):
-        self.assertEqual('3 3 3 3', resolve(points=((3, 3))))
+        self.assertEqual('3 3 3 3', resolve(points=((3, 3), )))
+
+    def test_seccond_example(self):
+        self.assertEqual('-10 -10 30 30', resolve(circles=((10, 10, 20), (20, 20, 10))))
 
 if __name__ == '__main__':
     unittest.main()
