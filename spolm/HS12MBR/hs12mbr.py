bd0c05bcaff073b636d06a8f46d3f8450e7df2fc
Joao Junior <joaojunior.ma@gmail.com>
2015-12-18 17:51:13 -0500

    Read input from line command and execute

--- spolm/HS12MBR/hs12mbr.py
+++ spolm/HS12MBR/hs12mbr.py
@@ -1,3 +1,5 @@
+import sys
+
 def resolve(points=(), lines=(), circles=()):
     x_min = 1000
     x_max = -1000
@@ -21,3 +23,23 @@ def resolve(points=(), lines=(), circles=()):
         if point[1] > y_max:
             y_max = point[1]
     return '{0} {1} {2} {3}'.format(x_min, y_min, x_max, y_max)
+
+if __name__ == '__main__':
+    numbers_tests = int(sys.stdin.next())
+    for test in xrange(numbers_tests):
+        quantity = int(sys.stdin.next())
+        points = []
+        lines = []
+        circles = []
+        for i in xrange(quantity):
+            line = sys.stdin.next()
+            line = line.split()
+            if line[0] == 'p':
+                points.append((int(line[1]), int(line[2])))
+            elif line[0] == 'l':
+                lines.append((int(line[1]), int(line[2]), int(line[3]), int(line[4])))
+            elif line[0] == 'c':
+                circles.append((int(line[1]), int(line[2]), int(line[3])))
+        print resolve(points, lines, circles)
+        if test + 1 < numbers_tests:
+            sys.stdin.next()
