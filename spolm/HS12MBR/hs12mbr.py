4f541c001377d05ebec6effc3788043e4d584a1a
Joao Junior <joaojunior.ma@gmail.com>
2015-12-18 17:30:22 -0500

    Refactoring to consider only two points in the circle.

--- spolm/HS12MBR/hs12mbr.py
+++ spolm/HS12MBR/hs12mbr.py
@@ -1,11 +1,16 @@
-def resolve(points=[], lines=(), circles=()):
+def resolve(points=(), lines=(), circles=()):
     x_min = 1000
     x_max = -1000
     y_min = 1000
     y_max = -1000
+    points = list(points)
     for line in lines:
         points.append((line[0], line[1]))
         points.append((line[2], line[3]))
+    for circle in circles:
+        ray = circle[2]
+        points.append((circle[0] - ray, circle[1] - ray))
+        points.append((circle[0] + ray, circle[1] + ray))
     for point in points:
         if point[0] < x_min:
             x_min = point[0]
@@ -15,14 +20,4 @@ def resolve(points=[], lines=(), circles=()):
             y_min = point[1]
         if point[1] > y_max:
             y_max = point[1]
-    for circle in circles:
-        ray = circle[2]
-        if circle[0] - ray < x_min:
-            x_min = circle[0] - ray
-        if circle[1] - ray < y_min:
-            y_min = circle[1] - ray
-        if circle[0] + ray > x_max:
-            x_max = circle[0] + ray
-        if circle[1] + ray > y_max:
-            y_max = circle[1] + ray
     return '{0} {1} {2} {3}'.format(x_min, y_min, x_max, y_max)
